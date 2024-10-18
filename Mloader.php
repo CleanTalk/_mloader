fd641bffaad4bacb8b2446484344ac2ab021f78c
Glomberg <bazz@bk.ru>
2024-10-18 10:40:05 +0300

    Added docblock.

--- Mloader.php
+++ Mloader.php
@@ -7,6 +7,26 @@ class Mloader
 	private static $common_namespace = '\Cleantalk\Common\\';
 	private static $custom_namespace = '\Cleantalk\Custom\\';
 
+    /**
+     * Retrieves the fully qualified class name of a module.
+     *
+     * This method attempts to find and return the class name of a module
+     * based on the provided module name. It first checks if the module
+     * exists in the custom namespace and if it is a subclass of the common
+     * namespace module. If not found, it checks the common namespace.
+     *
+     * @param string $module_name The name of the module to retrieve. It can include a namespace.
+     * @return string The fully qualified class name of the module.
+     * @throws \InvalidArgumentException If the module is not found or if the custom module is not a subclass of the common module.
+     *
+     * @example
+     * // Retrieve a simple class
+     * $class = Mloader::get('SimpleClass');
+     *
+     * @example
+     * // Retrieve a class with namespace
+     * $class = Mloader::get('Namespace\Class');
+     */
 	public static function get($module_name)
 	{
         $namespace = $module_name;
